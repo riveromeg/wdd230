63d33ebf20bd9b5bb9f3d9fd4ec947515792ebd5
MeganRH <riv21015@byui.edu>
2022-04-23 19:08:12 -0500

    al
    al

--- js/script.js
+++
@@ -1,7 +0,0 @@
-let d = new Date();
-
-document.getElementById("currentYear").textContent = d.getFullYear();
-
-let lastUpdated = document.lastModified;
-
-document.getElementById("updatedDate").textContent = lastUpdated;
