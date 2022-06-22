5d3cb4f1b8beb279fdbb7f57ee65d15bc6ccbd69
Megan G. Rivero <104280165+riveromeg@users.noreply.github.com>
2022-06-22 17:31:51 -0500

    api ulpoad

---
+++ lesson10/api-test/js/weatherapi.js
@@ -0,0 +1,13 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=Merida&appid=6c078ddf8469990df7fb60e22ba6957f';
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+    const desc = jsObject.weather[0].description;
+    document.getElementById('imagesrc').textContent = imagesrc;
+    document.getElementById('icon').setAttribute('src', imagesrc);
+    document.getElementById('icon').setAttribute('alt', desc);
+  });
