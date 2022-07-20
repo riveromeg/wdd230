7b5db7bdb9aa97533167a8af4668de10a0806abe
Megan G. Rivero <104280165+riveromeg@users.noreply.github.com>
2022-07-19 19:30:56 -0500

    upload
    final website upload

---
+++ final/js/main.js
@@ -0,0 +1,128 @@
+const requestURL = 'json/temples.json';
+const temples = document.querySelector(".maintemple");
+const cards = document.querySelector('.cards');
+
+// Calls functions to display temple data, then calls fetchWeather to pull weather data associated with that location
+async function apiFetch() {
+  let response = await fetch(requestURL);
+  if (response.ok) {
+    let data = await response.json();
+    let coordinates = displayTemples(data.temples);
+    let latitude = coordinates[0];
+    let longitude = coordinates[1];
+    // Creates weatherURL based on coordinates for the selected temple
+    const weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly&units=imperial&appid=3af250634d071cb3f84c2c5c48e9d30a`
+    // Calls fetchWeather using weatherURL associated with temple location
+    fetchWeather(weatherURL);
+  } else {
+    throw Error(response.statusText);
+  }
+}
+
+// Displays random temple and associated data on the homepage
+function displayTemples(data) {
+  // Create elements to add to the document
+  const randomIndex = Math.floor(Math.random() * data.length);
+  const item = data[randomIndex];
+  let card = document.createElement('div');
+  let name = document.createElement('h2');
+  let address = document.createElement('p');
+  let phone = document.createElement('p');
+  let email = document.createElement('p');
+  let image = document.createElement('img');
+  let history = document.createElement('p');
+
+  let latitude = item.latitude;
+  let longitude = item.longitude;
+
+  name.textContent = `Spotlighted Temple: ${item.name}`;
+  address.textContent = `Address: ${item.address}`;
+  phone.textContent = `Phone: ${item.phone}`;
+  email.textContent = `Email: ${item.email}`;
+  history.textContent = `History: ${item.longhistory}`;
+
+  // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+  image.setAttribute('src', item.image);
+  image.setAttribute('alt', `Outside image of ${item.name}`);
+  image.setAttribute('loading', 'lazy');
+
+  card.appendChild(name);
+  card.appendChild(image);
+  card.appendChild(address);
+  card.appendChild(phone);
+  card.appendChild(email);
+  card.appendChild(history);
+  card.classList.add('templecard')
+
+  // Add/append the existing HTML div with the cards class with the section(card)
+  document.querySelector('div.maintemple').appendChild(card);
+  return [latitude, longitude];
+};
+
+// Calls function to display weather forecasts based on provided coordinates
+async function fetchWeather(weatherURL) {
+  try {
+    const response = await fetch(weatherURL);
+    if (response.ok) {
+      const data = await response.json();
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Add weather forecasts to index.html page
+function displayResults(weatherData) {
+  for (let i = 0; i < 4; i++) {
+    let time = weatherData.daily[i];
+    let card = document.createElement('section');
+    let h3 = document.createElement('h3');
+    let temperature = document.createElement('p');
+    let humidity = document.createElement('p');
+    let image = document.createElement('img');
+    let weather = document.createElement('p');
+
+    const date = new Intl.DateTimeFormat("en-US", {
+      dateStyle: "full"
+    }).format(new Date(time.dt * 1000));
+
+    if (i == 0) {
+      h3.textContent = `Today's Weather`;
+    } else {
+      h3.textContent = `Forecast for ${date}`;
+    }
+
+    const desc = toTitleCase(time.weather[0].description);
+    temperature.textContent = `Temp: ${time.temp.day.toFixed(0)} °F`;
+    humidity.textContent = `Humidity: ${time.humidity}%`;
+    weather.textContent = desc
+
+    image.setAttribute('src', `images/weather/${time.weather[0].icon}.png`)
+    image.setAttribute('alt', desc)
+    image.setAttribute('loading', 'lazy');
+    image.setAttribute('width', '128px');
+    image.setAttribute('height', '128px');
+
+    card.classList.add('weathercard')
+
+    card.appendChild(h3);
+    card.appendChild(image)
+    card.appendChild(weather)
+    card.appendChild(temperature);
+    card.appendChild(humidity);
+
+    cards.appendChild(card);
+  };
+}
+
+// Capitalizes words in the weather description
+function toTitleCase(str) {
+  return str.toLowerCase().split(' ').map(function (word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
+}
+
+apiFetch()
---
+++ final/js/temples.js
@@ -0,0 +1,119 @@
+const requestURL = 'json/temples.json';
+const temples = document.querySelector(".temples");
+
+async function getTemples() {
+  let response = await fetch(requestURL);
+  if (response.ok) {
+    let data = await response.json();
+    displayTemples(data);
+  } else {
+    throw Error(response.statusText);
+  }
+}
+
+function displayTemples(data) {
+  // Create elements to add to the document
+  data.temples.forEach(temple => {
+    let card = document.createElement('div');
+    let name = document.createElement('h3');
+    let address = document.createElement('p');
+    let phone = document.createElement('p');
+    let email = document.createElement('p');
+    let image = document.createElement('img');
+    let button = document.createElement('button');
+
+    let services = document.createElement('p');
+    let history = document.createElement('p');
+    let ordinances = document.createElement('p');
+    let session = document.createElement('p');
+    let closure = document.createElement('p');
+
+    name.textContent = `${temple.name}`;
+    address.textContent = `Address: ${temple.address}`;
+    phone.textContent = `Phone: ${temple.phone}`;
+    email.textContent = `Email: ${temple.email}`;
+    services.textContent = `Services: ${temple.services}`;
+    history.textContent = `History: ${temple.history}`;
+    ordinances.textContent = `Ordinance Schedule: ${temple.ordinanceschedule}`;
+    session.textContent = `Sessions: ${temple.sessionschedule}`;
+    closure.textContent = `Closed: ${temple.templeclosureschedule}`;
+    button.textContent = `Click to like this Temple`;
+
+    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+    image.setAttribute('src', temple.image);
+    image.setAttribute('alt', `Outside image of ${temple.name}`);
+    image.setAttribute('loading', 'lazy');
+
+    button.classList.add('linkbutton');
+    button.classList.add('bluebackground');
+
+    if (temple.name == "Laie Temple") {
+      button.classList.add('laiebutton')
+      button.setAttribute('onclick', 'laieCounter()');
+    } else if (temple.name == "Washington D.C. Temple") {
+      button.classList.add('wdcbutton')
+      button.setAttribute('onclick', 'wdcCounter()');
+    } else if (temple.name == "Salt Lake Temple") {
+      button.classList.add('slbutton')
+      button.setAttribute('onclick', 'slCounter()');
+    } else if (temple.name == "San Diego California Temple") {
+      button.classList.add('sdbutton')
+      button.setAttribute('onclick', 'sdCounter()');
+    }
+
+    card.appendChild(name);
+    card.appendChild(image);
+    card.appendChild(button);
+    card.appendChild(address)
+    card.appendChild(phone);
+    card.appendChild(email);
+    card.appendChild(services);
+    card.appendChild(history);
+    card.appendChild(ordinances);
+    card.appendChild(session);
+    card.appendChild(closure);
+    card.classList.add('templecard')
+
+
+    // Add/append the existing HTML div with the cards class with the section(card)
+    document.querySelector('div.temples').appendChild(card);
+  });
+}
+
+getTemples()
+
+function laieCounter() {
+  if (localStorage.laiecount) {
+    localStorage.laiecount = Number(localStorage.laiecount) + 1;
+  } else {
+    localStorage.laiecount = 0;
+  }
+  document.getElementById("laiecount").innerHTML = localStorage.laiecount;
+}
+
+function slCounter() {
+  if (localStorage.slcount) {
+    localStorage.slcount = Number(localStorage.slcount) + 1;
+  } else {
+    localStorage.slcount = 0;
+  }
+  document.getElementById("slcount").innerHTML = localStorage.slcount;
+}
+
+function sdCounter() {
+  if (localStorage.sdcount) {
+    localStorage.sdcount = Number(localStorage.sdcount) + 1;
+  } else {
+    localStorage.sdcount = 0;
+  }
+  document.getElementById("sdcount").innerHTML = localStorage.sdcount;
+}
+
+function wdcCounter() {
+  if (localStorage.wdccount) {
+    localStorage.wdccount = Number(localStorage.wdccount) + 1;
+  } else {
+    localStorage.wdccount = 0;
+  }
+  document.getElementById("wdccount").innerHTML = localStorage.wdccount;
+}
